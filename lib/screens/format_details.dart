24ac0c4dd218665e7b6d78c1659ed7df1f512c3f
Shimamura Yuta <shimamura@agora-c.com>
2023-06-14 15:45:01 +0900

    20230614 commit

--- lib/screens/format_details.dart
+++ lib/screens/format_details.dart
@@ -2,8 +2,6 @@ import 'package:data_chest_exe/common/functions.dart';
 import 'package:data_chest_exe/common/style.dart';
 import 'package:data_chest_exe/models/format.dart';
 import 'package:data_chest_exe/screens/backup_source.dart';
-import 'package:data_chest_exe/screens/scanner.dart';
-import 'package:data_chest_exe/screens/scanner2.dart';
 import 'package:data_chest_exe/services/backup.dart';
 import 'package:data_chest_exe/services/format.dart';
 import 'package:data_chest_exe/services/log.dart';
@@ -346,36 +344,6 @@ class _FormatDetailsScreenState extends State<FormatDetailsScreen> {
                                   backupService: backupService,
                                   format: widget.format,
                                   getBackups: _getBackups,
-                                ),
-                              ),
-                            ),
-                            const SizedBox(width: 8),
-                            CustomIconTextButton(
-                              iconData: FluentIcons.generic_scan,
-                              iconColor: whiteColor,
-                              labelText: 'スキャナー',
-                              labelColor: whiteColor,
-                              backgroundColor: blueColor,
-                              onPressed: () => Navigator.push(
-                                context,
-                                FluentPageRoute(
-                                  builder: (context) => const ScannerScreen(),
-                                  fullscreenDialog: true,
-                                ),
-                              ),
-                            ),
-                            const SizedBox(width: 8),
-                            CustomIconTextButton(
-                              iconData: FluentIcons.generic_scan,
-                              iconColor: whiteColor,
-                              labelText: 'スキャナー2',
-                              labelColor: whiteColor,
-                              backgroundColor: blueColor,
-                              onPressed: () => Navigator.push(
-                                context,
-                                FluentPageRoute(
-                                  builder: (context) => const Scanner2Screen(),
-                                  fullscreenDialog: true,
                                 ),
                               ),
                             ),
