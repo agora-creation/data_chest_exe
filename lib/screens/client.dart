6d49749b433d4ea85db3339bdaab7ff1ea442f98
Shimamura Yuta <shimamura@agora-c.com>
2023-07-04 18:59:14 +0900

    20230704 commit

---
+++ lib/screens/client.dart
@@ -0,0 +1,15 @@
+import 'package:fluent_ui/fluent_ui.dart';
+
+class ClientScreen extends StatefulWidget {
+  const ClientScreen({super.key});
+
+  @override
+  State<ClientScreen> createState() => _ClientScreenState();
+}
+
+class _ClientScreenState extends State<ClientScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
